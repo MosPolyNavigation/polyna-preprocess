5fc4fa6c4bc4531c6b8583bdd31ac1c45a8a9c00
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-10 18:05:20 +0300

    Extract corpuses

---
+++ navigationData/v2/extractors/makeCorpuses.ts
@@ -0,0 +1,23 @@
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+import {Keys} from '../../configs/Keys.ts';
+import config from '../../configs/config.json';
+import {getSheetRows, minifyJSON, toBoolean} from '../../functions/commons.js';
+import {Corpus} from '../types.js';
+
+export async function makeCorpuses(spreadsheet: GoogleSpreadsheet): Promise<Corpus[]> {
+    let rows = await getSheetRows(spreadsheet, config.sheetsIDs.corpuses);
+    let corpuses = rows.map(row => {
+        const corpus: Corpus = {
+            id: row.get(Keys.id),
+            title: row.get(Keys.title),
+            available: toBoolean(row.get(Keys.available)),
+            locationId: row.get(Keys.corpuses.location)
+        };
+        if (corpus.available && row.get(Keys.corpuses.stairs)) {
+            corpus.stairs = JSON.parse(minifyJSON(row.get(Keys.corpuses.stairs)));
+        }
+        return corpus
+    });
+    console.log('Корпуса заполнены\n');
+    return corpuses;
+}
