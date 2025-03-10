68be74d396353b7e2b549d7c439fca9546240b92
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-10 18:26:04 +0300

    export rooms

---
+++ navigationData/v2/extractors/makeRooms.ts
@@ -0,0 +1,24 @@
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+import {Keys} from '../../configs/Keys.ts';
+import config from '../../configs/config.json';
+import {getSheetRows, toBoolean} from '../../functions/commons.js';
+import {Room} from '../types.js';
+
+export async function makeRooms(spreadsheet: GoogleSpreadsheet): Promise<Room[]> {
+    const rows = await getSheetRows(spreadsheet, config.sheetsIDs.rooms);
+    const rooms = rows.map(row => {
+        const room: Room = {
+            id: row.get(Keys.id),
+            type: row.get(Keys.rooms.type),
+            available: toBoolean(row.get(Keys.available)),
+            numberOrTitle: row.get(Keys.rooms.numberOrTitle),
+            tabletText: row.get(Keys.rooms.tabletText),
+            addInfo: row.get(Keys.rooms.addInfo),
+            planId: row.get(Keys.rooms.plan),
+        };
+        return room;
+    });
+    console.log('Помещения (аудитории) заполнены\n');
+    return rooms;
+}
+
