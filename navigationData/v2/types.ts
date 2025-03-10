f3701cfa0909050fe4b33b886a8210e11819220b
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-10 18:04:58 +0300

    v2 types

---
+++ navigationData/v2/types.ts
@@ -0,0 +1,48 @@
+export type Location = {
+    id: string
+    title: string
+    short: string
+    available: boolean
+    address: string
+    // corpuses?: Corpus[]
+    crossings?: string
+}
+
+export type Corpus = {
+    id: string
+    // locationID: string
+    title: string
+    available: boolean
+    // plans?: Plan[]
+    stairs?: string
+    locationId: string
+}
+
+export type Plan = {
+    // rooms?: Room[];
+    id: string
+    // corpusID: string
+    floor: string
+    available: boolean
+    wayToSvg: string
+    graph: string
+    entrances: string
+    corpusId: string
+}
+
+export type Room = {
+    planId: string
+	id: string
+	type: string
+	available: boolean
+	numberOrTitle: string
+	tabletText: string
+	addInfo: string
+}
+
+export type ExportData = {
+    locations: Array<Location>
+    corpuses: Array<Corpus>
+    plan: Array<Plan>
+    room: Array<Room>
+}
